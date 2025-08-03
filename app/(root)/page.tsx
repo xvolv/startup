e363f5d0d93ec4e57ccaf29e2ae69bd15f8b1261
salhadin <salassefa@gmail.com>
2025-08-03 04:05:47 +0300

    search filterning

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,9 +9,9 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
-   
+   const params = {search: query || null};
 
-  const {data: posts} = await sanityFetch({ query: STARTUPS_QUERY });
+  const {data: posts} = await sanityFetch({ query: STARTUPS_QUERY, params });
 
 
   return (
--- sanity/lib/quries.ts
+++ sanity/lib/quries.ts
@@ -1,14 +1,28 @@
 import { defineQuery } from "next-sanity";
-export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
+
+export const STARTUPS_QUERY = defineQuery(`*[
+  _type == "startup" &&
+  defined(slug.current) &&
+  (
+    !defined($search) ||
+    catagory match $search ||
+    title match $search ||
+    description match $search ||
+    author->name match $search
+  )
+] | order(_createdAt desc){
   _id,
   title,
   slug,
   _createdAt,
   author->{
-    _id,name,image,bio
+    _id,
+    name,
+    image,
+    bio
   },
   views,
   description,
   catagory,
   image
-}`)
+}`);
